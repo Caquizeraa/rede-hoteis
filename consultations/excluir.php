ac99275994e9d14fa5a8a63a6d07ed9f92163288
Otávio Sbampato <otaviosbampato@gmail.com>
2024-08-20 23:44:58 -0300

    Finalização PHP

--- consultations/excluir.php
+++ consultations/excluir.php
@@ -1,8 +1,13 @@
 <?php
 include("./config.php");
-$con = mysqli_connect($host, $login, $senha, $bd);
-$sql = "DELETE FROM Cliente WHERE idCliente=" . $_GET["codigo"];
-mysqli_query($con, $sql);
+try {
+    $con = mysqli_connect($host, $login, $senha, $bd);
+    $sql = "DELETE FROM Cliente WHERE idCliente=" . $_GET["codigo"];
+    mysqli_query($con, $sql);
+    } catch () {
+        <script>window.alert("Não podemos excluir clientes com reservas!")</script>
+}
+
 mysqli_close($con);
 header("location: ./index.php");
 ?>
